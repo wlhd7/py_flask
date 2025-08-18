804b385c509f45ca9708d38d96f99ff0a980eba4
wlhd <3509896163@qq.com>
2025-08-18 10:16:19 +0800

    HTML Escaping
     On branch master
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>hello world</p>"
+
+
+@app.route("/<name>")
+def hello(name):
+    return f"hello {escape(name)}"
 
 
 if __name__ == "__main__":
