181f4d5205932b2fd4cf6b4b4bed0a07e6e5531c
wlhd <3509896163@qq.com>
2025-08-18 13:55:40 +0800

    Unique URLs / Redirection Behavior
    The following two rules differ in their use of a trailing slash: /project/, /about
    The canonical URL for the projects endpoint has a trailing slash. It’s similar to a folder in a file system. If you access the URL without a trailing slash (/projects), Flask redirects you to the canonical URL with the trailing slash (/projects/).
    
    The canonical URL for the about endpoint does not have a trailing slash. It’s similar to the pathname of a file. Accessing the URL with a trailing slash (/about/) produces a 404 “Not Found” error. This helps keep URLs unique for these resources, which helps search engines avoid indexing the same page twice.
    
     On branch master
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -26,5 +26,15 @@ def show_subpath(subpath):
 # /path/<script>alert("injection attack")</script>
 
 
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
