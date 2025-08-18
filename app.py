dad7a7295aa477546db466e3fd83c2bda2468158
wlhd <3509896163@qq.com>
2025-08-18 15:57:15 +0800

    URL Building
    To build a URL to a specific function, use the url_for() function. It accepts the name of the function as its first argument and any number of keyword arguments, each corresponding to a variable part of the URL rule. Unknown variable parts are appended to the URL as query parameters.
    
    Why would you want to build URLs using the URL reversing function url_for() instead of hard-coding them into your templates?
    
        1. Reversing is often more descriptive than hard-coding the URLs.
    
        2. You can change your URLs in one go instead of needing to remember to manually change hard-coded URLs.
    
        3. URL building handles escaping of special characters transparently.
    
        4. The generated paths are always absolute, avoiding unexpected behavior of relative paths in browsers.
    
        5. If your application is placed outside the URL root, for example, in /myapplication instead of /, url_for() properly handles that for you.
    
     On branch master
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -34,6 +34,28 @@ def projects():
 @app.route('/about')
 def about():
     return 'The about page'
+
+
+@app.route('/login')
+def login():
+    return 'login'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{escape(username)}\'s name'
+
+
+@app.route('/http-request/<username>')
+def http_request(username):
+    print(url_for('http_request', username='wlhd'))
+    return 'http request'
+
+
+with app.test_request_context():
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 
 if __name__ == "__main__":
