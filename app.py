2d9339daf71f2749fb4acfa7100549110a042544
wlhd <3509896163@qq.com>
2025-08-20 16:24:04 +0800

    Flask.test_request_context() - Content Manager
    On branch master
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
-from flask import Flask
-import pytest
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'index'
-
-
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
-
-
-def test_index(client):
-    response = client.get('/')
-    assert response.status_code == 200
+with app.test_request_context('/hello', method='POST'):
+    assert request.path == '/hello'
+    assert request.method == 'POST'
