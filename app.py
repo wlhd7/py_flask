3a8d3b5ad7b471783aa37ed89a99bdd5be52588e
wlhd <3509896163@qq.com>
2025-08-20 08:10:46 +0800

    Application Context Example
    To access objects exposed by the Application and Request contexts
    outside of a view function, you need to create the appropriate context
    first:
    
     On branch master
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, request
+from flask import Flask, current_app
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    print(current_app.config['DEBUG'])
+    return 'index'
+
+
+# Without a context manager
+app_ctx = app.app_context()
+app_ctx.push()
+print(current_app.config['DEBUG'])
+app_ctx.pop()
+
+
+# With a context manager
 with app.app_context():
-    with app.test_request_context("/?name=Alice"):
-        print(request.args)
-    with app.test_request_context("/?name=Bob"):
-        print(request.args)
+    print(current_app.config['DEBUG'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
