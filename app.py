bff7f5165941bb2ef4f2eca391b32e7df43854cc
wlhd <3509896163@qq.com>
2025-08-21 16:21:27 +0800

    Visit Counter V1
     On branch master
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    resp = make_response(render_template('index.j2'))
-#    resp.set_cookie('username', 'wlhd')
+def counter():
+    # 使用 get() 方法，如果 visits 不存在，不会报错，并给 visits 赋值: 0
+    count = int(request.cookies.get('visits', 0)) + 1
+    resp = make_response(f'''
+        <p>You have visit this site {count} times</p>
+        <a href="/reset">Reset Counter</a>
+    ''')
+    resp.set_cookie('visits', str(count))
+    return resp
+
+
+@app.route('/reset')
+def reset():
+    resp = make_response('''
+        <p>Counter has been reset</p>
+        <a href="/">Restart counter</a>
+    ''')
+    # 将名为 visits 的 cookie 的值设为空支付串，将过期时间设置为过去的时间（立即过期） == 立即删除该 cookie
+    resp.set_cookie('visits', '', expires=0)
     return resp
